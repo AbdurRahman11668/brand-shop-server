9923d44800f469684b6772316edecb59df19c046
Abdur Rahman <abdur1222.ar@gmail.com>
2023-10-21 15:13:57 +0600

    completed

--- Index.js
+++ Index.js
@@ -30,7 +30,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-// https://i.ibb.co/3dfwcYp/ezgif-com-webp-to-jpg-removebg.png
 
 async function run() {
   try {
@@ -108,7 +107,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
