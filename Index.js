de39ad424f2b1c2f11faf2143728eeba77deacef
Abdur Rahman <abdur1222.ar@gmail.com>
2023-10-21 15:29:12 +0600

    completed

--- Index.js
+++ Index.js
@@ -21,7 +21,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@technobrandshop.hyfvgpd.mongodb.net/?retryWrites=true&w=majority`;
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,7 +32,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const technologyCollection = client.db("technoDB").collection("products");
@@ -107,7 +105,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
